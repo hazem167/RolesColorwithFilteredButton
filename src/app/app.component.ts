4789247c117c615b5015e5f296e3cca4ccccd41e
hazemah <hazemah654321@example.com>
2025-09-09 22:30:36 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';  // ✅ correct import
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatCard } from '@angular/material/card';
+import { MatChip } from '@angular/material/chips';
+import { NgClass, NgFor,NgForOf, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports:[MatFormField,MatLabel,MatCard,MatChip,NgFor,NgIf,FormsModule,NgForOf,MatInput,NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fetchedData';
+export class AppComponent implements OnInit {
+  users: any[] = [];
+  filteredUsers: any[] = [];
+  searchValue: string = '';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe(data => {
+      console.log('Loaded users:', data);
+      this.users = data;
+      this.filteredUsers = data;
+    });
+  }
+
+  search() {
+    this.filteredUsers = this.users.filter(user =>
+      user.email.toLowerCase().includes(this.searchValue.toLowerCase())
+    );
+  }
+
+  getRoleColor(role: string): 'primary' | 'accent' | 'warn' {
+    switch (role) {
+      case 'admin':
+        return 'warn';
+      case 'moderator':
+        return 'primary';
+      case 'user':
+        return 'primary';
+      default:
+        return 'primary';
+    }
+  }
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatIconModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
